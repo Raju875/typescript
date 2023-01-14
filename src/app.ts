258c867f79421d52ac503c8b3b2a0cf423a694ed
Raju875 <alaminraju875@gmail.com>
2023-01-14 11:46:49 +0600

    tsc script add in package.json

---
+++ src/app.ts
@@ -0,0 +1,2 @@
+let appMessage: string = "I am your app.ts->app.js";
+console.log(appMessage);
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+let indexMessage: string = "I am your index.ts->index.js";
+console.log(indexMessage);
